6c6d66dc812041a2993632e68bfa050c68b2a88e
hamza <hamza.abidi1@esprit.tn>
2022-04-04 12:12:53 +0100

    utilitaires

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -11,12 +11,12 @@
             <template v-slot:content>
                 <div class="p-fluid">
                     <div class="field">
-                        <label for="firstname">Name</label>
+                        <label for="firstname">Vendor Name</label>
                         <InputText id="firstname" v-model="firstname" :class="{'p-invalid': validationErrors.firstname && submitted}" />
                         <small v-show="validationErrors.firstname && submitted" class="p-error">Firstname is required.</small>
                     </div>
                     <div class="field">
-                        <label for="lastname">Subname</label>
+                        <label for="lastname">Vendor Subname</label>
                         <InputText id="lastname" v-model="lastname" :class="{'p-invalid': validationErrors.lastname && submitted}" />
                         <small v-show="validationErrors.lastname && submitted" class="p-error">Lastname is required.</small>
                     </div>
@@ -25,50 +25,6 @@
                         <InputNumber id="phone" v-model="phone" :class="{'p-invalid': validationErrors.phone && submitted}" />
                         <small v-show="validationErrors.phone && submitted" class="p-error">Phone Number is required.</small>
                     </div>
-                    <div class="field">
-                        <label for="username">Vendor Name</label>
-                        <InputText id="username" v-model="username" :class="{'p-invalid': validationErrors.username && submitted}" />
-                        <small v-show="validationErrors.username && submitted" class="p-error">Vendor Name is required.</small>
-                    </div>
-
-                    <div class="field">
-                    <label for="password">Password</label>
-                    <Password v-model="password" id="password" :class="{'p-invalid': validationErrors.password && submitted}" >
-                    <template #header>
-                        <h6>Pick a password</h6>
-                    </template>
-                    <template #footer>
-                        <p class="mt-2">Suggestions</p>
-                        <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
-                            <li>At least one lowercase</li>
-                            <li>At least one uppercase</li>
-                            <li>At least one numeric</li>
-                            <li>Minimum 8 characters</li>
-                        </ul>
-                    </template>
-                </Password>
-                <small v-show="validationErrors.password && submitted" class="p-error">password is required.</small>
-                </div>
-
-                <div class="field">
-                    <label for="confirmpassword">Confirm Password</label>
-                    <Password v-model="confirmpassword" id="confirmpassword" :class="{'p-invalid': validationErrors.confirmpassword && submitted}" >
-                    <template #header>
-                        <h6>Confirm password</h6>
-                    </template>
-                    <template #footer>
-                        <p class="mt-2">Suggestions</p>
-                        <ul class="pl-2 ml-2 mt-0" style="line-height: 1.5">
-                            <li>At least one lowercase</li>
-                            <li>At least one uppercase</li>
-                            <li>At least one numeric</li>
-                            <li>Minimum 8 characters</li>
-                        </ul>
-                    </template>
-                </Password>
-                <small v-show="validationErrors.confirmpassword && submitted" class="p-error">Confirm password is required or is wrong.</small>
-                </div>
-
                 </div>
             </template>
             <template v-slot:footer>
@@ -109,10 +65,7 @@ export default {
         return {
             firstname: '',
             lastname: '',
-            username: '',
-            password: '',
             token: '',
-            confirmpassword: '',
             phone: null,
             submitted: false,
             validationErrors: {}
@@ -126,7 +79,7 @@ export default {
             this.submitted = true;
             if (this.validateForm()) {  
                 if(! this.existename())
-                    this.$emit('next-page', {formData: {firstname: this.firstname, lastname: this.lastname, username: this.username,phone: this.phone,password: this.password,token: this.$route.query}, pageIndex: 0});
+                    this.$emit('next-page', {formData: {firstname: this.firstname, lastname: this.lastname,phone: this.phone,token: this.$route.query}, pageIndex: 0});
                 else
                    { console.log("user existe")
                    this.$toast.add({severity:'error', summary: 'Error Message', detail:'UserName already taken', life: 3000});}
@@ -141,20 +94,7 @@ export default {
                 this.validationErrors['lastname'] = true;
             else
                 delete this.validationErrors['lastname'];
-                        if (!this.username.trim())
-                this.validationErrors['username'] = true;
-            else
-                delete this.validationErrors['username'];
-                        if (!this.password.trim())
-                this.validationErrors['password'] = true;
-            else
-                delete this.validationErrors['password'];
-                        if (!this.confirmpassword.trim() && this.password.toString != this.confirmpassword.toString )
-                this.validationErrors['confirmpassword'] = true;            
-            else
-                 delete this.validationErrors['confirmpassword'];
-            
-                  if (this.phone === null)
+            if (this.phone === null)
                 this.validationErrors['phone'] = true;
             else
                 delete this.validationErrors['phone'];
