ea6232d8470bc5a3a62d5be55f97539456d6dd42
hamza <hamza.abidi1@esprit.tn>
2022-04-21 10:16:42 +0100

    rfq po invoice

--- src/components/TablesFournisseur.vue
+++ src/components/TablesFournisseur.vue
@@ -1,62 +1,87 @@
 <template>
-
     <div>
         <div class="card">
             <TabView>
-                <TabPanel v-for="tab in tabs" :key="tab.title" :header="tab.title">
-                    <p>{{tab.content}}</p>
-                </TabPanel>
-            </TabView>
+	<TabPanel header="RFQ LIST">
+		<div>
+        <DataTable  :value="rfq" responsiveLayout="scroll">
+            <Column field="code" header="Code"></Column>
+            <Column field="name" header="Name"></Column>
+            <Column field="category" header="Category"></Column>
+            <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+    </div>
+	</TabPanel>
+	<TabPanel header="PO LIST">
+		<div>
+        <DataTable :value="po" responsiveLayout="scroll">
+            <Column field="code" header="Code"></Column>
+            <Column field="name" header="Name"></Column>
+            <Column field="category" header="Category"></Column>
+            <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+    </div>
+	</TabPanel>
+	<TabPanel header="INVOICE LIST">
+		<div>
+        <DataTable :value="invoice" responsiveLayout="scroll">
+            <Column field="code" header="Code"></Column>
+            <Column field="name" header="Name"></Column>
+            <Column field="category" header="Category"></Column>
+            <Column field="quantity" header="Quantity"></Column>
+        </DataTable>
+    </div>
+	</TabPanel>
+</TabView>
         </div>
-
     </div>
 </template>
-
 <script>
-import { ref } from 'vue';
+
 import TabView from 'primevue/tabview';
 import TabPanel from 'primevue/tabpanel';
+import Column from 'primevue/column';
+import DataTable from 'primevue/datatable';
+import VendorService from '../services/VendorService';
 
 export default {
     components: {
    TabView,
-   TabPanel
+   TabPanel,
+   Column,
+   DataTable
   },
-    setup() {
-        const active1 = ref(0);
-        const active2 = ref(0);
-        const tabs = ref([
-            {
-                title: 'RFQ list',
-                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."
-            },
-            {
-                title: 'PO LIST ',
-                content: "Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Consectetur, adipisci velit, sed quia non numquam eius modi."
-            },
-            {
-                title: 'INVOICE LIST',
-                content: "At vero eos et accusamus et iusto odio dignissimos ducimus qui blanditiis praesentium voluptatum deleniti atque corrupti quos dolores et quas molestias excepturi sint occaecati cupiditate non provident, similique sunt in culpa qui officia deserunt mollitia animi, id est laborum et dolorum fuga. Et harum quidem rerum facilis est et expedita distinctio. Nam libero tempore, cum soluta nobis est eligendi optio cumque nihil impedit quo minus."
-            }
-        ]);
-        const scrollableTabs = ref(Array.from({ length: 50 }, (_, i) => ({ title: `Tab ${i + 1}`, content: `Tab ${i + 1} Content` })));
-
-        return { active1, active2, tabs, scrollableTabs }
+  data() {
+        return {
+            po: null,
+            invoice: null,
+            rfq: null
+        }
+  },
+   vendorService: null,
+    created() {
+        this.vendorService = new VendorService();
+    },
+    mounted() {
+        let jsonobject= localStorage.user;
+        let monobjet = JSON.parse(jsonobject)
+        this.vendorService.getpo(monobjet.username).then(data => this.po = data);
+        console.log(this.vendorService.getpo(monobjet.username).then(data => this.po = data))
+        this.vendorService.getinvoice(monobjet.username).then(data2 => this.invoice = data2);
+        this.vendorService.getrfq(monobjet.username).then(data3 => this.rfq = data3);
     }
+    
 }
 </script>
-
 <style lang="scss" scoped>
 .tabview-custom {
     i, span {
         vertical-align: middle;
     }
-
     span {
         margin: 0 .5rem;
     }
 }
-
 .p-tabview p {
     line-height: 1.5;
     margin: 0;
